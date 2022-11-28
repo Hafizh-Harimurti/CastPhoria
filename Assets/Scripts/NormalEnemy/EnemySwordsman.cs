12bfd5e7254fd836d97da400638ed2cdc8546851
adityusufp <91403022+adityusufp@users.noreply.github.com>
2022-11-29 03:56:13 +0700

    fix a few stuff

---
+++ Assets/Scripts/NormalEnemy/EnemySwordsman.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySwordsman : EntityBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnUpdate();
+        targetPos = target.transform.position;
+        Move();
+    }
+
+    private void LateUpdate()
+    {
+        OnLateUpdate();
+    }
+
+    void Move()
+    {
+        relativePos = targetPos - transform.position;
+        if (relativePos.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (relativePos.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    void Attack()
+    {
+        float angle = Mathf.Atan2(relativePos.y, relativePos.x) * Mathf.Rad2Deg;
+        Instantiate(arrow, transform.position, Quaternion.AngleAxis(angle, Vector3.forward));
+        animator.SetBool("isAttacking", false);
+    }
+}
