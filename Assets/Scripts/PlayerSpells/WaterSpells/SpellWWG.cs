9b0b47f987398854e1bc67c6566c3bb8bebd1fc5
adityusufp <91403022+adityusufp@users.noreply.github.com>
2022-11-29 21:08:54 +0700

    readd spell

---
+++ Assets/Scripts/PlayerSpells/WaterSpells/SpellWWG.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellWWG : SpellBase
+{
+    public float stunDuration;
+
+    private Vector2 knockbackForce;
+    private List<GameObject> entitiesHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        entitiesHit = new List<GameObject>();
+        StartCoroutine(EndSpell(2));
+    }
+
+    private void Update()
+    {
+        if (owner == null) Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        GameObject otherGameObject = collider.gameObject;
+        if (!otherGameObject.CompareTag(owner.tag) && !otherGameObject.CompareTag("Projectile") && !otherGameObject.CompareTag("Spell"))
+        {
+            entitiesHit.Add(collider.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        entitiesHit.Remove(collider.gameObject);
+    }
+
+    IEnumerator EndSpell(float lifetime)
+    {
+        yield return new WaitForSeconds(lifetime);
+        GetComponent<Animator>().SetBool("isDone", true);
+    }
+
+    void DamageEntity()
+    {
+        EntityBase entity = null;
+        foreach (GameObject otherGameObject in entitiesHit)
+        {
+            entity = otherGameObject.GetComponent<EntityBase>();
+            entity.TakeDamage(damage);
+            entity.ApplyDebuff(Debuff.Stun, stunDuration, 1);
+            knockbackForce = (transform.position - castOrigin).normalized * 0.2f;
+        }
+    }
+
+    void DestroySpell()
+    {
+        Destroy(gameObject);
+    }
+}
